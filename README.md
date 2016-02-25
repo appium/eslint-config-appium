64876742dc252bde42cbf068a90a037bb2bec344
Isaac Murchie <isaac@saucelabs.com>
2016-02-25 10:32:45 -0800

    Initial configuration

---
+++ README.md
@@ -0,0 +1,13 @@
+## Appium ESLint Shared Configuation
+
+This package works to provide [eslint](http://eslint.org/) [shared configuration](http://eslint.org/docs/developer-guide/shareable-configs) to replace the need for a local `.eslintrc` file.
+
+It uses [babel-eslint](https://github.com/babel/babel-eslint) as a parser.
+
+### Peer Dependencies
+
+This config requires the following packages be installed in your project:
+
+* [eslint](https://www.npmjs.com/package/eslint)
+* [eslint-plugin-babel](https://www.npmjs.com/package/eslint-plugin-babel)
+* [eslint-plugin-import](https://www.npmjs.com/package/eslint-plugin-import)
---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      impliedStrict: true,
+      experimentalObjectRestSpread: true
+    },
+  },
+  env: {
+    node: true,
+    mocha: true,
+    es6: true
+  },
+  plugins: [
+    "babel",
+    "import"
+  ],
+  rules: {
+    semi: [2, 'always'],
+    'dot-notation': 2,
+    eqeqeq: [2, 'smart'],
+    'brace-style': [2, '1tbs', {allowSingleLine: true}],
+    'comma-spacing': [2, {before: false, after: true}],
+    indent: [2, 2, {VariableDeclarator: 2, SwitchCase: 1}],
+    'arrow-spacing': 2,
+    'keyword-spacing': 2,
+    'comma-dangle': 0,
+    'no-empty': 0,
+    'babel/array-bracket-spacing': 1,
+    'babel/object-shorthand': 1,
+    'babel/arrow-parens': 1,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors'
+  ]
+}
